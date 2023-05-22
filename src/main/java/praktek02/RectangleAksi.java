d4984e9f0c73b8c53dd8219264f7ea7e1f85e878
Alzahsi1212 <alzahsi1212@gmail.com>
2023-05-22 17:31:09 +0800

    Mengisi nilai Atribute

--- src/main/java/praktek02/RectangleAksi.java
+++ src/main/java/praktek02/RectangleAksi.java
@@ -4,6 +4,8 @@ public class RectangleAksi {
     public static void main(String[] args) {
         
         Rectangle rl = new Rectangle();
+        rl.panjang = 6;
+        rl.lebar = 2;
     }
     
 }
